8fb466bbbcc9536c4e89ca6ff7d7e329cdcf4e63
Maruf Ahmed <mk6116055@gmail.com>
2022-11-20 03:37:53 +0600

    add product page

--- lib/pages/add_product_page.dart
+++ lib/pages/add_product_page.dart
@@ -1,8 +1,50 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 
-class AddProductPage extends StatelessWidget {
+import '../models/category_model.dart';
+import '../utils/helper_functions.dart';
+
+class AddProductPage extends StatefulWidget {
   static const String routeName = '/addproduct';
+
   const AddProductPage({Key? key}) : super(key: key);
+
+  @override
+  State<AddProductPage> createState() => _AddProductPageState();
+}
+
+class _AddProductPageState extends State<AddProductPage> {
+  final _nameController = TextEditingController();
+  final _shortDescriptionController = TextEditingController();
+  final _longDescriptionController = TextEditingController();
+  final _purchasePriceController = TextEditingController();
+  final _salePriceController = TextEditingController();
+  final _discountController = TextEditingController();
+  final _quantityController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String? thumbnailImageUrl;
+  CategoryModel? categoryModel;
+  DateTime? purchaseDate;
+  late StreamSubscription<ConnectivityResult> subscription;
+  bool _isConnected = true;
+
+  @override
+  void initState() {
+    isConnectedToInternet().then((value) {
+      setState(() {
+        _isConnected = value;
+      });
+    });
+    subscription = Connectivity().onConnectivityChanged.listen((result) {
+      setState(() {
+        _isConnected = result == ConnectivityResult.wifi ||
+            result == ConnectivityResult.mobile;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +52,35 @@ class AddProductPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Add Product'),
       ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: [
+            if (!_isConnected)
+              const ListTile(
+                tileColor: Colors.red,
+                title: Text(
+                  'No internet connectivity',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _shortDescriptionController.dispose();
+    _longDescriptionController.dispose();
+    _purchasePriceController.dispose();
+    _quantityController.dispose();
+    _discountController.dispose();
+    _salePriceController.dispose();
+    subscription.cancel();
+    super.dispose();
   }
 }
