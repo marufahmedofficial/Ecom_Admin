12fc8de6c32425b24895ef483d3f060516092dd8
Maruf Ahmed <mk6116055@gmail.com>
2022-11-20 03:55:38 +0600

    add product design

--- lib/pages/add_product_page.dart
+++ lib/pages/add_product_page.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 import '../models/category_model.dart';
+import '../providers/product_provider.dart';
 import '../utils/helper_functions.dart';
 
 class AddProductPage extends StatefulWidget {
@@ -65,10 +70,246 @@ class _AddProductPageState extends State<AddProductPage> {
                   style: TextStyle(color: Colors.white),
                 ),
               ),
+            Consumer<ProductProvider>(
+              builder: (context, provider, child) => DropdownButtonFormField<CategoryModel>(
+                hint: const Text('Select Category'),
+                value: categoryModel,
+                isExpanded: true,
+                validator: (value) {
+                  if(value == null) {
+                    return 'Please select a category';
+                  }
+                  return null;
+                },
+                items: provider.categoryList.map((catModel) =>
+                    DropdownMenuItem(
+                        value: catModel,
+                        child: Text(catModel.categoryName))).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    categoryModel = value;
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  labelText: 'Enter Product Name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'This field must not be empty';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: TextFormField(
+                maxLines: 2,
+                controller: _shortDescriptionController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  labelText: 'Enter Short Description(optional)',
+                ),
+                validator: (value) {
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: TextFormField(
+                maxLines: 3,
+                controller: _longDescriptionController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  labelText: 'Enter Long Description(optional)',
+                ),
+                validator: (value) {
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                controller: _purchasePriceController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  labelText: 'Enter Purchase Price',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'This field must not be empty';
+                  }
+                  if (num.parse(value) <= 0) {
+                    return 'Price should be greater than 0';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                controller: _salePriceController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  labelText: 'Enter Sale Price',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'This field must not be empty';
+                  }
+                  if (num.parse(value) <= 0) {
+                    return 'Price should be greater than 0';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                controller: _quantityController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  labelText: 'Enter Quantity',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'This field must not be empty';
+                  }
+                  if (num.parse(value) <= 0) {
+                    return 'Quantity should be greater than 0';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                controller: _discountController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  labelText: 'Enter Discount',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'This field must not be empty';
+                  }
+                  if (num.parse(value) < 0) {
+                    return 'Discount should not be a negative value';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton.icon(
+                      onPressed: _selectDate,
+                      icon: const Icon(Icons.calendar_month),
+                      label: const Text('Select Purchase Date'),
+                    ),
+                    Text(purchaseDate == null
+                        ? 'No date chosen'
+                        : getFormattedDate(purchaseDate!)),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Card(
+                      child: thumbnailImageUrl == null ? const Icon(Icons.photo, size: 100,) :
+                      Image.file(File(thumbnailImageUrl!), width: 100, height: 100, fit: BoxFit.cover,),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton.icon(
+                          onPressed: () {
+                            _getImage(ImageSource.camera);
+                          },
+                          icon: const Icon(Icons.camera),
+                          label: const Text('Open Camera'),
+                        ),
+                        TextButton.icon(
+                          onPressed: () {
+                            _getImage(ImageSource.gallery);
+                          },
+                          icon: const Icon(Icons.photo_album),
+                          label: const Text('Open Gallery'),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  void _selectDate() async {
+    final date = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(DateTime.now().year - 1),
+      lastDate: DateTime.now(),
+    );
+    if(date != null) {
+      setState(() {
+        purchaseDate = date;
+      });
+    }
+  }
+
+  void _getImage(ImageSource imageSource) async {
+    final pickedImage = await ImagePicker().pickImage(source: imageSource, imageQuality: 70,);
+    if(pickedImage != null) {
+      setState(() {
+        thumbnailImageUrl = pickedImage.path;
+      });
+    }
+  }
+
+  void _saveProduct() async {
+    if(thumbnailImageUrl == null) {
+      showMsg(context, 'Please select a product image');
+      return;
+    }
+
+    if(purchaseDate == null) {
+      showMsg(context, 'Please select a purchase date');
+      return;
+    }
+
+    if(_formKey.currentState!.validate()) {
+      EasyLoading.show(status: 'Please wait');
+    }
   }
 
   @override
