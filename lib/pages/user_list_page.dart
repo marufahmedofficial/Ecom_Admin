1bd0c62500cd611fb76a73a1067e87e0d69f732c
Maruf Ahmed <mk6116055@gmail.com>
2023-03-16 20:42:17 +0600

    notification page

--- lib/pages/user_list_page.dart
+++ lib/pages/user_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/user_provider.dart';
+import '../utils/helper_functions.dart';
 
 class UserListPage extends StatelessWidget {
   static const String routeName = '/users';
--- lib/utils/helper_functions.dart
+++ lib/utils/helper_functions.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 showMsg(BuildContext context, String msg) =>
-    ScaffoldMessenger.of(context)
-    .showSnackBar(SnackBar(content: Text(msg)));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
 
 getFormattedDate(DateTime dt, {String pattern = 'dd/MM/yyyy'}) =>
     DateFormat(pattern).format(dt);
 
 Future<bool> isConnectedToInternet() async {
   final result = await Connectivity().checkConnectivity();
-  return result == ConnectivityResult.wifi || result == ConnectivityResult.mobile;
+  return result == ConnectivityResult.wifi ||
+      result == ConnectivityResult.mobile;
+}
+
+Duration getDifference(DateTime dt) {
+  return DateTime.now().difference(dt);
 }
