48d1940e5c70259c1b9f58607a1fff6d4a1759a1
Maruf Ahmed <mk6116055@gmail.com>
2023-03-16 20:38:44 +0600

    order details page

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../providers/order_provider.dart';
 import '../utils/constants.dart';
 import '../utils/helper_functions.dart';
+import 'order_details_Page.dart';
 
 class OrderPage extends StatelessWidget {
   static const String routeName = '/order';
