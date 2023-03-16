92fc8130c4572e8deced0f7ba788e1638ed73ddc
Maruf Ahmed <mk6116055@gmail.com>
2023-03-16 20:31:01 +0600

    constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,33 @@
 const String currencySymbol = '৳';
+const cities = [
+  'Dhaka',
+  'Chittagong',
+  'Rajshahi',
+  'Khulna',
+  'Barishal',
+  'Sylhet',
+  'Comilla',
+  'Noakhali',
+  'Faridpur',
+  'Rangpur',
+  'Gazipur'
+];
+
+abstract class OrderStatus {
+  static const String pending = 'Pending';
+  static const String processing = 'Processing';
+  static const String delivered = 'Delivered';
+  static const String cancelled = 'Cancelled';
+  static const String returned = 'Returned';
+}
+
+abstract class PaymentMethod {
+  static const String cod = 'Cash on Delivery';
+  static const String online = 'Online Payment';
+}
+
+abstract class NotificationType {
+  static const String comment = 'New Comment';
+  static const String order = 'New Order';
+  static const String user = 'New User';
+}
